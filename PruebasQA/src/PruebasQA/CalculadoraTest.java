1b75799068eaa5a2bab4517e90e56b797ca1121b
Elias Ortiz <elortiz@uaa.edu.py>
2025-10-21 20:40:39 -0300

    Pruebas Unitarias con JUnit

--- PruebasQA/src/PruebasQA/CalculadoraTest.java
+++ PruebasQA/src/PruebasQA/CalculadoraTest.java
@@ -1,26 +1,22 @@
 package PruebasQA;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
-	@Test
-	public void testSumar() {
-		Calculadora calc = new Calculadora();
-		int resultado = calc.sumar(5, 3);
-		assertEquals(8, resultado);
-	}
-	
-	
-	
-	@Test
-	public void testRestar() {
-		Calculadora calc = new Calculadora();
-		int resultado = calc.restar(10, 4);
-		assertEquals(6, resultado);
-	}
+    @Test
+    void testSumar() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.sumar(5, 3);
+        assertEquals(8, resultado);
+    }
 
+    @Test
+    void testRestar() {
+        Calculadora calc = new Calculadora();
+        int resultado = calc.restar(10, 4);
+        assertEquals(6, resultado);
+    }
 }
