03b5870be2dc285778cfa4dcd551b3da8bac0415
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-12-10 21:39:08 -0300

    correcion de conexion a la BD

--- src/PruebaQA/Main.java
+++ src/PruebaQA/Main.java
@@ -2,16 +2,10 @@ package PruebaQA;
 
 public class Main {
     public static void main(String[] args) {
-          UsuarioDAO dao = new UsuarioDAO();
-          dao.listarUsuarios(); // LLamamos al metodo ListarUsaurios
-          dao.actualizarUsuario(); 
-          
-          // → Crear un usuario con datos nuevos
-          Usuario u = new Usuario();
-          u.setid(5); // ID del usuario que querés actualizar
-          u.setNombre("Elías Ortiz");
-          u.setEmail("eliasortiz@example.com");
-          u.setContrasena("nuevo123");
-       
+          if ( Conexion.getConnection() != null) {
+        	  System.out.println("Conexion exitosa a Base de datos PostgreSQL !");
+          }  else {
+        	  System.out.println("No se  pudo conectar");
+          }
     }
 }
